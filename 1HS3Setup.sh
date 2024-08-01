9cd7fca7ea5f563b4871ce15f586be3670a3079e
IsabelThompson97 <ithomps3@nd.edu>
2024-08-01 13:46:48 -0400

    1HS3 setup

--- 1HS3Setup.sh
+++ 1HS3Setup.sh
@@ -4,7 +4,8 @@ source leaprc.RNA.OL3
 source leaprc.water.tip3p
 mol = loadpdb 1HS3out.pdb
 addions mol Na+ 0
-solvateoct mol TIP3PBOX 12.0
+solvateoct mol TIP3PBOX 10.0
+addionsrand mol Na+ 44 Cl- 44
 savepdb mol 1HS3final.pdb
 saveamberparm mol 1HS3.prmtop 1HS3.inpcrd
 saveamberparm mol 1HS3.prmtop 1HS3.rst7
