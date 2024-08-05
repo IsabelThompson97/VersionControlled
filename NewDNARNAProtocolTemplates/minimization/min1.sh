0474cbf283bfc4c942b5d2ae72ca8f866e8f45c1
IsabelThompson97 <ithomps3@nd.edu>
2024-08-05 12:42:21 -0400

    commit

--- NewDNARNAProtocolTemplates/minimization/min1.sh
+++ NewDNARNAProtocolTemplates/minimization/min1.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#$ -M nkarpins@nd.edu
+#$ -M ithomps3@nd.edu
 #$ -m abe
 #$ -pe smp 24
 #$ -q long
-#$ -N check
+#$ -N min1check
 
 module load amber/22.0
 
-mpirun -np $NSLOTS $AMBERHOME/bin/sander.MPI  -O -i min1.in -o min1.out -p final_double_bound.prmtop -c final_double_bound.rst7 -r min1.rst -inf min1.mdinfo -ref final_double_bound.rst7
+mpirun -np $NSLOTS $AMBERHOME/bin/sander.MPI  -O -i min1.in -o min1.out -p ggc.prmtop -c ggc.rst7 -r min1.rst -inf min1.mdinfo -ref ggc.rst7
--- NewDNARNAProtocolTemplates/minimization/min2.sh
+++ NewDNARNAProtocolTemplates/minimization/min2.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -M nkarpins@nd.edu
+#$ -M ithomps3@nd.edu
 #$ -m abe
 #$ -pe smp 24
 #$ -q long
@@ -7,5 +7,5 @@
 
 module load amber/22.0
 
-mpirun -np $NSLOTS $AMBERHOME/bin/sander.MPI -O -i min2.in -o min2.out -p final_double_bound.prmtop -c min1.rst -r min2.rst -inf min2.mdinfo -ref min1.rst
+mpirun -np $NSLOTS $AMBERHOME/bin/sander.MPI -O -i min2.in -o min2.out -p ggc.prmtop -c min1.rst -r min2.rst -inf min2.mdinfo -ref min1.rst
 
