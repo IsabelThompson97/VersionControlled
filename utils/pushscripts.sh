1b5aebefd36740464f684888479f22eb1e92d01a
IsabelThompson97 <ithomps3@nd.edu>
2025-05-19 10:56:08 -0400

    update pushscripts util

--- utils/pushscripts.sh
+++ utils/pushscripts.sh
@@ -5,14 +5,14 @@
 # Script to copy **scripts only** to GitHub controlled folder
 # DO NOT cp data files
 # RUN ME FROM THE PROJECT DIRECTORY SCRIPTS FOLDER
-# /afs/crc.nd.edu/user/i/ithomps3/VersionControlled/utils/pushscripts.sh $OutputName #$CommitMessage
+# /users/ithomps3/VersionControlled/utils/pushscripts.sh $OutputName 
 # With alias in .bashrc command is **** pushscripts $OutputName *******
 # Example: pushscripts RNAHairpins/GGC/Analysis/process_cpptraj
 # -------------------
 
-ISABEL_GIT_DIR="/afs/crc.nd.edu/user/i/ithomps3/VersionControlled/"
+ISABEL_GIT_DIR="/users/ithomps3/VersionControlled/"
 ARG_1=$1
-#COMMIT_MESSAGE=$2
+
 
 STORE_AT=$ISABEL_GIT_DIR$ARG_1
 
@@ -27,8 +27,3 @@ cp *.ipynb $STORE_AT
 cp *.py $STORE_AT
 
 # ------------
-
-# Figure out how to automatically fetch GitHub 
-
-#echo "git add $ISABEL_GIT_DIR"
-#echo 'git commit -c '$ISABEL_GIT_DIR' -m "'"$COMMIT_MESSAGE"'"'
