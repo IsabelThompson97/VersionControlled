81bf487fb6b561a8867df7ecc913ca56e9c6f77f
IsabelThompson97 <ithomps3@nd.edu>
2026-02-21 20:15:09 -0500

    commit

--- utils/binviz/westpa_visualizer.py
+++ utils/binviz/westpa_visualizer.py
@@ -67,7 +67,7 @@ class BinSchemeParser:
                 continue
             
             # Look for variable assignments
-            if '=' in line and any(kw in line.lower() for kw in ['rmsd', 'mindist', 'bins', 'spacing', 'Quad', 'RMSD', 'MinDist']):
+            if '=' in line and any(kw in line.lower() for kw in ['rmsd', 'mindist', 'bins', 'spacing', 'quad', 'RMSD', 'MinDist','high','low']):
                 # Extract variable name
                 match = re.match(r'\s*(\w+)\s*=\s*(.+)', line)
                 if not match:
